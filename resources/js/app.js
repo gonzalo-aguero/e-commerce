4ad1ce853083cf74fcdd3e1570fc59fc19b2cb80
Gonzalo Aguero <gonzaloaguerodev@gmail.com>
2023-03-02 12:49:45 -0300

    -

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,3 +5,9 @@ import Alpine from 'alpinejs';
 window.Alpine = Alpine;
 
 Alpine.start();
+
+console.log("Gonzalo's code is working!");
+
+fetch('./Productos.json')
+  .then(response => response.json())
+  .then(data => console.log(data));
